be06cda5fd49040c14bcf6c54ca15583c5dbeca9
Ricardo Buffa Verçosa <ricardobuffa@gmail.com>
2022-05-23 08:12:12 -0300

    acabandosecao07

---
+++ secao07_poo/129_classe01.js
@@ -0,0 +1,40 @@
+//Orientação objeto de javascript é centrada em função
+//A classe é um detalhe de nomenclatura ela é convertida pra função
+
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0){
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach( l => {
+            valorConsolidado += l.valor
+        })
+
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento('Salario', 45000)
+const contaDeLuz = new Lancamento('Luz', -220)
+
+const contaDeAgua = new Lancamento('Agua', -50)
+
+const contas = new CicloFinanceiro(6, 2018)
+contas.addLancamentos( salario, contaDeLuz, contaDeAgua)
+
+console.log(contas.sumario())
