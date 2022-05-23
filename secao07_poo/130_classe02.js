48be2a8ed7c5cce41fab9217a69107530cdab7fd
Ricardo Buffa Verçosa <ricardobuffa@gmail.com>
2022-05-23 09:46:17 -0300

    vaiqvai

---
+++ secao07_poo/130_classe02.js
@@ -0,0 +1,24 @@
+//Herança continua sendo via prototype
+
+
+class Avo {
+    constructor(sobrenome) {
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo {
+    constructor(sobrenome, profissao = 'Professor') {
+        super(sobrenome)
+        this.profissao = profissao
+    }
+}
+
+class Filho extends Pai {
+    constructor() {
+        super('Silva')
+    }
+}
+
+const filho = new Filho
+console.log(filho)
